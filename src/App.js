bca0f511d39e96719600788aa39bd4b3a1c89697
bouyyah <hamameayyoub@gmail.com>
2021-06-21 00:44:17 +0100

    fixed height and same message error

--- src/App.js
+++ src/App.js
@@ -4,21 +4,30 @@ import axios from 'axios';
 
 class App extends React.Component {
     
-        state = { advice : ''  };
-      
+    state = { advice : ''  };
+    lastAdvice = 'lastAdvice';  
+    currentAdvice = 'currentAdvice';
+
     componentDidMount(){
         this.fetchAdvice();
     }
 
-    fetchAdvice(){
+    fetchAdvice(){ 
+
         axios.get('https://api.adviceslip.com/advice')
-            .then((response) => {
-                const {advice} = response.data.slip;
-                this.setState({advice});
-            })
-            .catch((error)=>{
-                console.log(error);
-            });
+        .then(response => {       
+            this.lastAdvice = this.currentAdvice;    
+            this.currentAdvice = response.data.slip.advice;    
+            this.setState({advice : this.currentAdvice});
+            console.log("current :" + this.currentAdvice , "/n Last :" +this.lastAdvice );
+            if (this.currentAdvice === this.lastAdvice){this.fetchAdvice()}; 
+        })
+        .catch((error)=>{
+            console.log(error);
+        });
+            
+        
+      
     }
 
     render(){
